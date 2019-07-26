c36d68fb9e886c5847a89df7941a453d85efe7c8
Duan Shiqiang <duanshiqiang1@gmail.com>
2019-07-26 14:24:51 +0800

    Fix load balancer not able to reach ec2 instance

--- lib/ros/be/infra/files/terraform/aws/ec2/main.tf
+++ lib/ros/be/infra/files/terraform/aws/ec2/main.tf
@@ -18,6 +18,14 @@ resource "aws_security_group" "lb" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  # allow load balancer to reach out to instances
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "ec2" {
