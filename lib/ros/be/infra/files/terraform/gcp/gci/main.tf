23e21ceb9a6488f66b212c547d621ca609c30e4f
Kostya <kostya@perxtech.com>
2019-08-24 01:03:44 +0800

    Updated GCP terraform templates, startup script file

--- lib/ros/be/infra/files/terraform/gcp/gci/main.tf
+++ lib/ros/be/infra/files/terraform/gcp/gci/main.tf
@@ -1,3 +1,15 @@
+ locals {
+  disk_image_map = {
+    debian = "debian-cloud/debian-9"
+    ubuntu = "ubuntu-os-cloud/ubuntu-1804-lts"
+  }
+
+  user_map = {
+    debian = "admin"
+    ubuntu = "ubuntu"
+  }
+}
+
 resource "google_compute_instance" "this" {
   name         = var.name
   machine_type = var.machine_type
@@ -5,12 +17,18 @@ resource "google_compute_instance" "this" {
   
   boot_disk {
     initialize_params {
-      image = var.disk_image
+      image = local.disk_image_map[var.disk_image]
     }
   }
 
   network_interface {
     subnetwork = var.subnetwork   
     access_config {}
-  }  
+  }
+
+  metadata_startup_script = templatefile("${path.module}/templates/startup-script.tpl", {
+    project_name   = var.project_name,
+    user           = local.user_map[var.disk_image],
+    ssh_public_key = var.ssh_public_key
+  })
 }
