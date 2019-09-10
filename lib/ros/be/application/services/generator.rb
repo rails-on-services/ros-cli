e13ccf9469569e110c47853cd13d7de0e8d11210
Kostya <kostya@perxtech.com>
2019-09-10 18:30:34 +0800

    Refactor Fluentd config generator

--- lib/ros/be/application/services/generator.rb
+++ lib/ros/be/application/services/generator.rb
@@ -56,11 +56,7 @@ module Ros
             @fluentd ||= Config::Options.new({
               header: cluster.infra.cluster_type.eql?('kubernetes') ? "configMaps:\n  ros.conf: |" : '',
               include_tcp_source: cluster.infra.cluster_type.eql?('kubernetes') ? false : true,
-              # log_tag: "#{api_hostname}.rack-traffic-log",
-              request_log_tag: "**.rack-traffic-log",
-              event_log_tag: "events-log.**",
               kafka_brokers: cluster.infra.cluster_type.eql?('kubernetes') ? 'kafka:9092' : 'kafkastack:9092',
-              # storage_name: "storage#{base_hostname.gsub('.', '-')}",
               current_feature_set: application.current_feature_set
             }).merge!(application.components.services.components[:'fluentd'])
             binding.pry
