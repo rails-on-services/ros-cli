4e0df9ce18f1f136959622c4e724fb07550163db
Kostya <kostya@perxtech.com>
2019-09-27 15:41:37 +0800

    [WHIS-62] Refactor helm_release related resources

--- lib/ros/be/infra/files/terraform/aws/eks-resources/outputs.tf
+++ lib/ros/be/infra/files/terraform/aws/eks-resources/outputs.tf
@@ -2,3 +2,11 @@
 output "istio_ingressgateway_alb_arn" {
   value = data.external.alb_arn.result.LoadBalancerArn
 }
+
+output "ros_repo" {
+  value = data.helm_repository.ros
+}
+
+output "incubator" {
+  value = data.helm_repository.incubator
+}
