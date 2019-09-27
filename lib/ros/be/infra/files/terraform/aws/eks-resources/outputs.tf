e63e2d1be6473d8d762b2440d561aeaa8003946b
Kostya <kostya@perxtech.com>
2019-09-28 03:34:15 +0800

    Remove test outputs

--- lib/ros/be/infra/files/terraform/aws/eks-resources/outputs.tf
+++ lib/ros/be/infra/files/terraform/aws/eks-resources/outputs.tf
@@ -2,11 +2,3 @@
 output "istio_ingressgateway_alb_arn" {
   value = data.external.alb_arn.result.LoadBalancerArn
 }
-
-output "ros_repo" {
-  value = data.helm_repository.ros
-}
-
-output "incubator" {
-  value = data.helm_repository.incubator
-}
